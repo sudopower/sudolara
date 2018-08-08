f58fb1e67f578a18962fcb44285478d2cd1a2d89
sudopower <nkirandroid@gmail.com>
2018-08-08 12:38:13 +0530

    posts page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -14,6 +14,7 @@
                   <li ><a href="/">Home</a></li>
                   <li><a href="/about">About</a></li>
                   <li><a href="/services">Services</a></li>
+                  <li><a href="/posts">Posts</a></li>
                 </ul>
               </div><!--/.nav-collapse -->
             </div>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    @if(count ($posts) > 1)
+            @foreach($posts as  $post)
+                <div class="well">
+                    <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+                    <small>Written on {{$post->created_at}}</small>
+                </div>
+            @endforeach
+    @else
+        <p>No posts found</p>
+    @endif
+@endsection
