abed3b7abff6c9cd0f8604a0e98c5fd7d5a08562
sudopower <nkirandroid@gmail.com>
2018-08-10 13:30:02 +0530

    access control

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,15 @@ use App\Post;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['index','show']]);
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -78,9 +87,12 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        $post = Post::find($id);
-        return view('posts.edit')->with('post',$post);
-
+        $post = Post::find($id);        
+        //Check for correct user
+        if (auth()->user()->id !== $post->user_id){
+            return redirect('/posts')->with('error','Unauthorized Page');
+        }
+        return redirect('posts.edit')->with('post',$post);
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,12 +7,16 @@
     <div class="jumbotron">
     <div class="container">{!!$post->body!!}</div>
     <hr>
-    <small>Written on {{$post->created_at}}</small>
+    <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
-    {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POST', 'class'=>'pull-right'])!!}
-        {{Form::hidden('_method','DELETE')}}
-        {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
-    {!!Form::close()!!}
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+                <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+                {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POST', 'class'=>'pull-right'])!!}
+                {{Form::hidden('_method','DELETE')}}
+                {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+                {!!Form::close()!!}
+        @endif
+    @endif
     </div>
 @endsection
